525913666fe20584a06c26a57bbc2f4bb186046d
KushDastane <kushdastane777@gmail.com>
2025-05-22 14:16:53 +0530

    updated html link rel again

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,7 @@ mongoose.set('strictQuery', false);
 app.use(cors());
 
 // Serve static frontend files and assets
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/auth', express.static(path.join(__dirname, '..', 'frontend', 'pages', 'Auth')));
 app.use(express.static(path.join(__dirname, '..', 'frontend')));
 app.use('/images', express.static(path.join(__dirname, '..', 'frontend', 'images')));
